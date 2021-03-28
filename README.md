181270c28d520fbdac8e5233ba440b1c80c461ca
apelloni <apelloni90@gmail.com>
2021-03-28 18:52:12 +0200

    init

---
+++ README.md
@@ -0,0 +1,190 @@
+#
+# cLTD - Causal Loop-Tree Duality
+**Authors**: Z. Capatti, V. Hirschi, D. Kermanschah, A. Pelloni, B. Ruijl
+![double box diagrams](./img/diags.svg)
+
+# Table of Contents
+1. [Introduction](#introduction)
+2. [Dependencies](#dependencies)
+4. [Usage](#usage)
+    1. [Import](#import)
+    2. [Options](#options)
+    2. [Input format](#input)
+    2. [Output format](#output)
+4. [Example](#example)
+    1. [Scalar integral: Double Box](#scalar-integral:-double-box)
+    2. [Raised power propagators: Triangle](#raised-power-propagators:-triangle)
+    3. [**Numerators**](#numerator)
+5. [Reference](#reference)
+
+
+## Introduction
+The integration of a `n`-loop Feynman diagram requires the integrating over `4*n` dimensions.
+By means of the Loop-Tree duality, it's possible to bring down this integration to only `3*n` spacial dimensions. 
+
+This new representation consists of the sum over all possible spanning tree of the original diagram. One of the major problems that one can encounter is that these terms will introduce a number of spurious singularities that could lead to a poor numerical integration convergence.
+
+Here we present a **Mathmatica Package** with the implementation of the algorithm presented in the paper [Manifestly Causal Loop-Tree Duality](https://arxiv.org/abs/2009.05509), where it was already implemented into a python script.
+
+## Dependencies
+* **Mathematica**: tested on Mathematica 2.0 and heigher.
+* **FORM**: can be downloaded from [here](https://www.nikhef.nl/~form/).
+
+## Usage
+
+### Import
+This package represents a Mathematica frontend and requires a working installation of [FORM](https://www.nikhef.nl/~form/) in order to run successfully.
+If the cLTD folder is added to the libraries in Mathematica, one can import it with:
+```Mathematica
+<< cLTD`
+```
+or alternatively:
+```Mathematica
+Get["<path-to-package-folder>/cLTD.m"]
+```
+
+
+
+### Options
+
+There are several options that can be set when calling the `cLTD` function. The default values are:
+```Mathematica 
+In[1]:= Options[cLTD]
+Out[1]= {"loopmom" -> {k0, k1, k2, k3}, 
+         "FORMpath" -> "form", 
+         "WorkingDirectory" -> NotebookDirectory[], 
+         "FORM_ID" -> None,
+         "keep_FORM_script" -> False, 
+         "EvalAll" -> False}
+```
+
+Their usage is as follows:
+
+* `loopmom`: defines the variables that need to be considered loop momenta in the evaluated expression
+
+* `"FORM_path"`: By default, it assumes that the path to `form` is included in `$PATH`. If this is not the case, one can set this option to the required path.
+This must be either a absolute or relative path (no `~`):
+```Mathematica
+SetOptions[cLTD, "FORM_path" -> "/home/dude/dir1/dir2/bin/form"]
+```
+
+* `WorkingDirectory`: FORM will need to create some temporary files in order to process the expression. By default, this happens in the same folder as the current Mathematica notebook.
+
+* `"FORM_ID"` and `"keep_FORM_script"`: the temporary file generate by the program have the name `cLTD_<FORM_ID>.frm` and `cLTD_<FORM_ID>.out`.
+By default, the `FORM_ID` is filled with a series of random characters in order to avoid possible conflicts with pre-existing files.
+As soon as the `cLTD` call retrieves the output from FORM it deletes these files.
+It may happen that you want to keep the script and also give it a more precise name.
+One way to do it is:
+```Mathematica
+SetOptions[cLTD, "FORM_ID" -> 1]
+SetOptions[cLTD, "keep_FORM_script" -> True]
+```
+
+* `EvalAll`: It substitute the `LTDnorm` and `den` functions.
+
+### Input
+
+The input must contain the **propagator** in the form:
+ - `prop[momentum, mass]`
+
+One can multiply several such propagators to build its own topology. One such use can be found in the [double-box example](#scalar-integral:-double-box), or even sum several topologies together. In the latter case, they all need to contain the same amount of loops.
+
+The **numerator** can be expressed by means of one helper function:
+ - `SP4[p1,p2]`: representation of the 4-dimensional scalar product between two momenta `p1` and `p2` using the metric `(1,-1,-1,-1)`.
+ 
+
+In case it's not possible to express the numerator in terms of scalar products, one can always write it as an energy polynomial.
+
+Some examples of valid numerator inputs (`k,l`: loop momenta, `p1,p2`: external momenta)
+```mathematica
+(*Unity*)
+num0 = 1;
+(*Using scalar products*)
+num1 = SP4[p1,k]+SP4[p2,k]+SP4[k,k];
+(*Energy polynomial*)
+num2 = k[0]*c1+l[0]*c2+k[0]*l[0]*c3+p1[0]*p2[0];
+(*Mixed*)
+num3 = k[0]*SP4[p1,l];
+```
+
+Note that only the energy components of the loop momenta are actively involved during the evaluation of the residue.
+
+### Output
+The output contains:
+ - `cLTDnorm[expr]`: normalization factor, it correponds to `1/expr`.
+ - `den[expr]`: denominators, can be evaluated as `1/expr`.
+ - `p.q`: Euclidean scalar product of the spacial component of two momenta
+ - `p[0]`: energy component of momentum `p`
+
+
+## Example
+
+### Scalar integral: Double Box
+For scalar propagators, one should use the helper function `prop[mom, mass]` in order to define the propagators that are present in the diagram:
+```Mathematica
+(* Get cLTD and define path to FORM *)
+<< cLTD`
+SetOptions[cLTD, "FORM_path" -> "/home/dude/dir1/dir2/bin/form"];
+
+(* Doube box *)
+expr = prop[k, 0] * prop[k-p1, 0] * prop[k+p2, 0] * prop[k-l,0]\
+     * prop[l+p2, 0] * prop[l+p2+p3, 0] * prop[l-p1, 0];
+
+(* Get the cLTD expression *)
+cDoubleBox = cLTD[expr, loopmom -> {k,l}];      
+```
+In order to numerically integrate the expression above in the remaining 6 dimensions, one will need to regulate the IR divergences.
+
+It is always possible to consider a massive box:
+```Mathematica
+(* Massive Doube box *)
+expr = prop[k, m] * prop[k-p1, m] * prop[k+p2, m] * prop[k-l,0]\
+     * prop[l+p2, m] * prop[l+p2+p3, m] * prop[l-p1, m];
+```
+
+### Raised power propagators: Triangle
+The code also allows for raised powers in the propagators. 
+For the case of a triangle with off-shell externals, one will need to regulate the UV divergences in order to obtain a finite integral:
+
+```Mathematica
+(* Get cLTD and define path to FORM *)
+<< cLTD`
+SetOptions[cLTD, "FORM_path" -> "/home/dude/dir1/dir2/bin/form"];
+
+(* TriangleBox - UV CT *)
+expr = prop[k, 0] * prop[k-p1, 0] * prop[k+p2, 0] - prop[k, mUV]^3;
+
+(* Get the cLTD expression *)
+cTriangleUV = cLTD[expr, loopmom -> {k}];      
+```
+
+### Numerators
+The code also supports numerators. 
+
+One can include **Minkowky scalar products** by using `SP4[mom1,mom2]` in the expression.
+
+For the one-loop photon self energy, the numerator takes the form:
+
+![](./img/bubble.svg)
+
+Which can be expressed as: 
+```mathematica
+(* One-loop photon self energy *)
+numerator = - 4 * SP4[k,p1] + 4 * m^2 \
+            + 8 * SP4[pol,k] * SP4[cpol,k]\
+            + 4 * SP4[pol,p1] * SP4[cpol,k]\
+            + 4 * SP4[pol,k] * SP4[cpol,p1];
+props = prop[k,m] * prop[k+p1,m];
+
+(* Get the cLTD expression *)
+cPhotonSelf = cLTD[numerator * props, loopmom -> {k}];      
+
+```
+
+It could be that the numerator cannot be written in terms of scalar products; in this case, one should write the numerator as a polynomial in the loop momenta's energy components.
+
+
+## Reference 
+If you use this program, please cite the corresponding work on which the procedure is based on:
+
+> Z. Capatti, V. Hirschi, D. Kermanschah, A. Pelloni, B. Ruijl, Manifestly Causal Loop-Tree Duality. arXiv: 2009.05509 [hep-ph] (Sept. 2020)
---
+++ cLTD.m
@@ -0,0 +1,347 @@
+(* ::Package:: *)
+
+(* ::Chapter:: *)
+(*cLTD package*)
+
+
+BeginPackage["cLTD`"];
+
+
+(* ::Section:: *)
+(*Global *)
+
+
+(* ::Subsection:: *)
+(*Usage*)
+
+
+cLTD::usage="\
+Compute the corresponding cLTD expression using FORM.
+"<>Style["Input Format",Bold]~ToString~StandardForm<>":
+The scalar propagators of the expression must be expressed as: 
+	-> prop[4momentum,mass] (e.g. prop[k1+p1, 0])
+
+Scalar products among 4-vectors in the numerator can be expressed as:
+    -> SP4[p1,p2]
+
+The numerator can also be a polynomial in the loop momenta's energy 
+components
+    -> e.g.: k[0]*p1[0] - p1[0]*p2[0] (k: loop momentum) 
+
+"<>Style["Options",Bold]~ToString~StandardForm<>": {
+          \"loopmom\"->{loopmomsymbol1,loopmomsymbol2,...} (default: {k1,k2,k3,k4})
+          \"FORMpath\"\[Rule]\"form\",
+          \"WorkingDirectory\"\[Rule]Directory[],
+          \"FORM_ID\"\[Rule]None,
+          \"keep_FORM_script\"\[Rule]False,
+          \"EvalAll\"\[Rule]False
+}
+	To change change the default options to different values use SetOptions[cLTD,...]:
+	e.g.:
+		Use `form` in the alphaloop directory "<>Style["SetOptions[cLTD,\"FORMpath\"\[Rule]\"/home/dude/dir1/dir2/form\"]",Bold]~ToString~StandardForm<>"
+
+"<>Style["Example",Bold]~ToString~StandardForm<>":
+ expr = SP4[k1,p1] prop[k1-p1,0]prop[k1-p2,0] - k1[0] p1[0] prop[k1-p3,m]prop[k1-p4,m]
+=> cLTD[expr]
+
+WARNING: when summing multiple diagrams, they must contain the same 
+         number of loops, if this is not possible, then call cLTD[] 
+         for each of them individually.
+
+"<>Style["Output",Bold]~ToString~StandardForm<>":
+  { cLTD expression, Energies definition} 
+  The expression contains the den[] and cLTDnorm[] function which are to be evaluated as 
+		den[a_] :> 1/a, cLTDnorm[a_] :> 1/a
+";
+
+Print[" :::::::::::::::::::::::: cLTD ::::::::::::::::::::::::"]
+Print["Authors: Z. Capatti, V. Hirschi, D. Kermanschah, A. Pelloni, B. Ruijl\n"];
+Print[cLTD::usage];
+
+
+(* ::Section:: *)
+(*Private*)
+
+
+Begin["cLTDPrivate`"];
+
+
+(* ::Subsection::Closed:: *)
+(*FORM file*)
+
+
+FORMhead = "Format 100;
+Auto S n,y,E,p,k;
+CF ncmd, num, Eres, Echain;
+CF den, prop, norm, error;
+CF x, xbar;
+
+set energies:E0,...,E1000;
+set shifts:p0,...,p1000;
+";
+
+
+FORMpf = "multiply norm(1)*num();
+repeat id norm(E?)*prop(y0?,y1?) = norm(2*E*y1)*(den(y0-y1)-den(y0+y1)); 
+.sort
+
+* NOTE:
+*    This procedure follows what presented in the paper 2009.05509
+*    The only difference is that the numerator function is that the
+*    divided difference is definded with a minus sing.
+*    As a result we have that each time a ncmd gain an extra term 
+*    we need to compensate with an additional minus sing.
+#do i=0,{`LOOPS'-1}
+* Normalise by the contour orientation 
+    id norm(n?) = norm(-n);
+  
+* Activate numerator
+    id num(?y) = num(1,?y,ncmd());
+
+* Identify poles in the loop momentum energy 
+    splitarg (k`i') den;
+    id den(?a,p?) = den(p,?a);
+    factarg (-1) den 1;
+    id den(p?,y?,p1?) = x(p1/y,0,0)/y;
+    id den(p?,y?) = den(y*p);
+    .sort:get-poles;
+
+* Split the poles by their position in the complex plane
+    splitarg x; 
+    .sort
+    id x(?y1,+E?energies,?y2,E1?,E2?) = x(?y1,?y2,E1+E,E2);
+    id x(?y1,-E?energies,?y2,E1?,E2?) = x(?y1,?y2,E1,E2+E);
+
+* check for errors
+    id x(?y,0,0) = error(x(?y,0,0));
+    id x(?y,E1?!{,0},E2?!{,0}) = error(x(?y, E1,E2));
+    if (count(error,1));
+         print \"[k`i'] Cannot find energy signature %t\";
+         exit \"Critical ERROR\";
+    endif;
+
+* re-absorb 
+    id x(?y,0,E?!{,0}) = x(?y,-E);
+    id x(?y,E?!{,0},0) = xbar(?y,E);
+    transform, x,xbar , addargs(1,last);
+    id x(y?) = x(-y);
+    id xbar(y?) = xbar(-y);
+*    print +s;
+
+* Remove elements with zero residue
+    if (count(x,1) == 0) discard;
+    if ( count(xbar,1) == 0 ) 
+        repeat id num(?n,ncmd(?n1))*x(y1?) = -num(?n,ncmd(?n1,y1));
+    .sort:x-xbar;
+
+    
+* Start unfolding the expression 
+    repeat;
+        if ( count(x,1) == 1 );
+            repeat id x(y1?)*xbar(y2?) = x(y1)*den(y1-y2);
+            id num(0,?n,ncmd(?n1))*x(y1?) = num(?n,ncmd(?n1));
+            id num(1,?n,ncmd(?n1))*x(y1?) = -num(?n,ncmd(?n1,y1));
+        endif;
+
+
+        id once num(0,?n,ncmd(?n1))*x(y1?)*xbar(y2?) = 
+                     -num(0,?n,ncmd(?n1))*Eres(y1,y2)*xbar(y2);
+    
+        id once num(1,?n,ncmd(?n1))*x(y1?)*xbar(y2?) = 
+                     +num(0,?n,ncmd(?n1,y1))*Eres(y1,y2)*xbar(y2)
+                     -num(1,?n,ncmd(?n1,y1))*xbar(y2);
+
+        repeat id Eres(y1?,y2?)*xbar(y2?)*xbar(y3?)= 
+                      den(y1-y2)*xbar(y3)*(xbar(y2)+Eres(y1,y3));
+        id once Eres(y1?,y2?)*xbar(y2?)= den(y1-y2)*xbar(y2);
+    endrepeat;
+    id num(n0?{0,1},?n) = num(?n);
+    .sort:recursive-expansion;
+
+* Sanity check
+    if (count(xbar,1) || count(x,1));
+         print \"[k`i'] Some roots were not absorbed: %t\";
+         exit \"Critical ERROR\";
+    endif;
+#enddo
+";
+
+
+FORMnum[nLoops_] := "*off statistics;
+CF ncmd, kval, num, a;
+
+
+Auto S invd;
+Auto S f,z,r,x,s,c;
+
+#$LOOPS="<>ToString[nLoops]<>";
+
+** Additional numerator function in LMB
+*#$numF = 1;
+** Multiply numerator functions
+*Multiply $numF;
+*print;
+.sort:start;
+
+* Start unfoding the numerator instructions loop by loop
+#do i = 0,{`$LOOPS'-1}
+    id once num(ncmd(?z),?x) =ncmd(?z,0)*num(?x);
+    B+ ncmd, k`i';
+    .sort:collect-ncmd;
+    keep brackets;
+    
+    id k`i'^r?*ncmd(?z,z1?,0) = sum_(s,0, r - nargs_(?z) + 0, a(r,s,?z)*z1^s);
+    B+ a;
+    .sort:collect-a;
+    keep brackets;
+
+    repeat id a(r?,s?,?z,z1?) = -sum_(k, s+1,r-nargs_(?z) + 0, a(r,k,?z)*z1^(k-s-1));
+    id a(r?,s?) = delta_(r,s);
+    .sort:energy-k`i';
+#enddo
+id num=1;
+
+* check that the substitution is complete
+if (count(ncmd, 1,num,1,a, 1));
+    Print \"Unsubstituted ncmd: %t\";
+    exit \"Critical error\";
+endif;
+.sort:rm-num;
+";
+
+
+FORMfile[filename_,expr_,nLoops_,FORMvars_]:=Module[{
+file="#--\n"},
+file = file <> FORMhead;
+file = file <> "Auto S sp,"<>StringRiffle[ToString/@FORMvars,","]<>";\n";
+file = file <> "#define LOOPS \""<>ToString[nLoops]<>"\"\n";
+file = file <> "#define topoID \""<>filename<>"\"\n";
+file = file <> "L F = "<>expr<>";\n";
+file = file <> FORMpf <> "\n.sort\n";
+file = file <> "L firstF = firstterm_(F);\n.sort\n#if ( termsin(firstF) != 0 )\n";
+file = file <> "#redefine oldextrasymbols \"`extrasymbols_'\"
+ExtraSymbols, underscore, den;
+hide firstF;
+argtoextrasymbol den;
+id den(y?) = y;
+.sort:den;\n#redefine newextrasymbols \"`extrasymbols_'\"\noff statistics;\n";
+file = file <> FORMnum[nLoops]; 
+file = file <> "#if `oldextrasymbols' != `newextrasymbols'\n";
+file = file <> "multiply replace_(<den{`oldextrasymbols'+1}_,den(extrasymbol_({`oldextrasymbols'+1}))>\\
+                  ,...,<den`extrasymbols_'_,den(extrasymbol_(`extrasymbols_'))>);\n";
+file = file <> "#endif\n";
+file = file <> "#endif\n.sort\nFormat mathematica;\n#write<"<>filename<>".out> \"%E\" F;\n.end";
+file
+]
+
+
+(* ::Subsection:: *)
+(*Functions*)
+
+
+toLTDprop[loopmom_]:=Module[{SP},
+	SetAttributes[SP,Orderless];
+	SP[p1_+p2_,p3_]:=SP[p1,p3]+SP[p2,p3];
+	SP[-p1_,p2_]:=-SP[p1,p2];
+	Global`prop[mom_?(!FreeQ[#,Alternatives@@loopmom]&),m_]:>LTDprop[
+		mom/.Table[v->v[0],{v,Complement[Variables[mom],loopmom]}],
+		Sqrt[SP[mom,mom]+m^2]/.SP->Dot]
+	];
+
+
+SetAttributes[SP4,Orderless];
+SP4[p1_+p2_,q_][loopmom_]:=SP4[p1,q][loopmom]+SP4[p2,q][loopmom]
+SP4[a_Integer p_,q_][loopmom_]:=a SP4[p,q][loopmom]
+SP4[p_,q_][loopmom_]:=If[FreeQ[loopmom,p],p[0],p]*If[FreeQ[loopmom,q],q[0],q] - p . q 
+
+
+Options[cLTD]={
+	"loopmom"->{Global`k0,Global`k1,Global`k2,Global`k3},
+	"FORMpath"->"form",
+	"WorkingDirectory"->NotebookDirectory[],
+	"FORM_ID"->None, 
+	"keep_FORM_script"->False,
+	"EvalAll"->False};
+cLTD[expression_,OptionsPattern[]]:=Module[{
+expr=If[Head[expression]===Plus,List@@expression, {expression}]/.Global`SP4[p1_,p2_]:> SP4[p1,p2][OptionValue["loopmom"]]/.toLTDprop[OptionValue["loopmom"]],
+energies,i=0,loop0subs, p0subs,spsubs,FORMinput, 
+filenameID,
+runfilename,cLTDfilename,
+FORMpath,
+result, return,cleanKs,cleanProps,
+FORMvars={}},
+
+(*Check that the reserved variables are not being used in the input*)
+If[!FreeQ[expression,Global`x|Global`xbar],Print["x and xbar are used internally by FORM. Pease use different variables in your expression"];Abort[]];
+If[!FreeQ[expression,Alternatives@@Table[ToExpression["E"<>ToString[i]],{i,0,Length[Union[Cases[expression,_prop,Infinity]]]-1}]],
+	Print["E# are reserved for the energies of the cLTD procedue. Pease use different variables in your expression"];Abort[]];
+
+PrintTemporary["Mapping variables for FORM expression"];
+(*Sanitisation*)
+cleanKs=Table[k->ToExpression["nonLoopk"<>ToString[i++]],{k, Complement[Table[ToExpression["k"<>ToString[n]],{n,0,9}],OptionValue["loopmom"]]}];i=0;
+cleanProps=Table[noLoopProp->ToExpression["noLoopProp"<>ToString[i++]],{noLoopProp,Union[Cases[expr,Global`prop[__],Infinity]]}];i=0;
+
+(*map FORM symbols to input values*)
+energies = Table[(While[!FreeQ[expr,ToExpression["E"<>ToString[i++]]],Null];ToExpression["E"<>ToString[i-1]])->e,{e,Last/@Union[Cases[expr,LTDprop[__],Infinity]]}];i=0;
+loop0subs = Table[l->ToExpression["LTDk"<>ToString[i++]],{l,Select[OptionValue["loopmom"],!FreeQ[expr,#]&]}];i=0;
+If[Length[loop0subs] == 0, Print["No loop momenta"]; Abort[]];
+p0subs = Table[p0->ToExpression["p0Component"<>ToString[i++]],{p0, Union[Cases[expr,p_?(FreeQ[#,Alternatives@@OptionValue["loopmom"]]&)[0],Infinity]]}];i=0;
+(*Print[cleanProps];*)
+expr = expr/.ReplaceAll[energies,Rule[a_,b_]:>Rule[b,a]]/.cleanProps/.cleanKs/.p0subs/.Table[k[0]->k,{k,OptionValue["loopmom"]}];
+
+(*Check for scalar products in the numerator*)
+spsubs = Table[x->(While[!FreeQ[expr,ToExpression["sp"<>ToString[i++]]],Null];ToExpression["sp"<>ToString[i-1]]),{x,Union[Cases[expr,_Dot,Infinity]]}];i=0;
+expr = expr/.spsubs/.loop0subs;
+
+(*Extract FORM variable*)
+FORMvars = Join[FORMvars, Union[Flatten[Variables[#[[1]]]&/@Union[Cases[expr,_LTDprop,Infinity]]]]/.{
+	Alternatives@@Last/@loop0subs:>Nothing,
+	Alternatives@@Last/@cleanKs:>Nothing
+	}];
+FORMvars = Join[FORMvars, Variables[expr]/.{
+	_LTDprop:>Nothing,
+	Alternatives@@Last/@spsubs:>Nothing
+	}];
+FORMvars = Union[FORMvars];
+
+(*Create strings to send to FORM*)
+PrintTemporary["Create FORM expression"];
+FORMinput=StringReplace[{"cLTDPrivate`"->"","LTD"->"","[":>"(","]"->")"}][ToString[Plus@@expr,InputForm]];
+(*Print[FORMinput];*)
+
+PrintTemporary["Execute FORM"];
+(*FORM Executable*)
+FORMpath=If[
+OptionValue["FORMpath"]=="form", "form",
+FileInformation[OptionValue["FORMpath"],"AbsoluteFileName"]
+];
+
+(*Call FORM*)
+filenameID=If[OptionValue["FORM_ID"]===None,
+	StringJoin@@RandomSample[Join[Alphabet[], ToUpperCase /@ Alphabet[]], 10],
+	OptionValue["FORM_ID"]
+];
+runfilename=OptionValue["WorkingDirectory"]<>"/cLTD_"<>ToString[filenameID]<>".frm";
+cLTDfilename=OptionValue["WorkingDirectory"]<>"/cLTD_out_"<>ToString[filenameID]<>".out";
+(*Print[{FORMpath,runfilename}];*)
+(*Print[FORMfile["cLTD_out_"<>ToString[filenameID],FORMinput,Length[loop0subs],alPATH]];*)
+Export[runfilename,FORMfile["cLTD_out_"<>ToString[filenameID],FORMinput,Length[loop0subs],FORMvars],"Text"];
+return = RunProcess[{FORMpath,runfilename},ProcessDirectory->OptionValue["WorkingDirectory"]];
+If[return["ExitCode"] != 0, Print[return]];
+
+PrintTemporary["Retrive result"];
+result=ToExpression[StringReplace[" "|"\\"|"\n"->""][Import[cLTDfilename,"Text"]]];
+If[!OptionValue["keep_FORM_script"],DeleteFile[{runfilename,cLTDfilename}]];
+PrintTemporary["Map back to mathematica variables"];
+result = result/.Reverse/@spsubs\
+				/.Reverse/@p0subs\
+				/.Reverse/@cleanKs\
+				/.Reverse/@cleanProps\
+				/.Global`norm->cLTD`cLTDnorm;
+				
+{Collect[result,_cLTDnorm], energies}//.If[OptionValue["EvalAll"],{Global`den[a_]:>1/a,cLTD`cLTDnorm[a_]:>1/a}, {}]
+]
+
+
+End[];
+EndPackage[];
